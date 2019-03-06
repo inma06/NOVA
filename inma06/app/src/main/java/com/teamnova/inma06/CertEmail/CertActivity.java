7afd2574d11eaa067303daac40a47945ae967e01
pakbongho <pakbongho@gmail.com>
2019-03-06 17:48:24 +0900

    이메일 인증 해야함

--- inma06/app/src/main/java/com/teamnova/inma06/CertEmail/CertActivity.java
+++ inma06/app/src/main/java/com/teamnova/inma06/CertEmail/CertActivity.java
@@ -5,34 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.teamnova.nova.R;
 
 public class CertActivity extends AppCompatActivity {
 
-  TextView userEmailTv;
-  TextView isCertEmailTv;
+  TextView userIdTv; //유저 아이디 텍스트 뷰
 
-  Button certBtn;
+  Button sendMailBtn; //인증메일 발송버튼
+  Button certBtn; // 인증하기 버튼
+
+  EditText certNumberEt;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_cert);
 
-    userEmailTv = findViewById(R.id.email_TV);
-    isCertEmailTv = findViewById(R.id.isCertTV);
+
+    userIdTv = findViewById(R.id.userID_TV);
+    certNumberEt = findViewById(R.id.certNumber_ET);
+    sendMailBtn = findViewById(R.id.sendMail_Btn);
     certBtn = findViewById(R.id.certBtn);
 
     Intent intent = getIntent();
     String userID = intent.getStringExtra("userID");
-    userEmailTv.setText(userID);
+    userIdTv.setText(userID);
+    //인텐트로 아이디를 받아와서 텍스트뷰에 뿌려줍니다.
 
     certBtn.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        final String userEmail = userEmailTv.getText().toString();
+        final String userEmail = userIdTv.getText().toString(); //메일을 발송할 userEmail입니다.
+
+
 
         //TODO: 이메일 인증부분
         /* 이메일 발송 -> 랜덤한 코드 4자리 발송 */
@@ -40,6 +48,8 @@ public class CertActivity extends AppCompatActivity {
 
       }
     });
+
+
 
 
 
