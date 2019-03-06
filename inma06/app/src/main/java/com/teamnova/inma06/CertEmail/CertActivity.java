0c19ce05170a3b26d68625f20a8ce7f16b9aaee8
pakbongho <pakbongho@gmail.com>
2019-03-06 14:30:37 +0900

    이메일 인증 해야함

--- inma06/app/src/main/java/com/teamnova/inma06/CertEmail/CertActivity.java
+++ inma06/app/src/main/java/com/teamnova/inma06/CertEmail/CertActivity.java
@@ -3,6 +3,8 @@ package com.teamnova.inma06.CertEmail;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.teamnova.nova.R;
@@ -10,7 +12,9 @@ import com.teamnova.nova.R;
 public class CertActivity extends AppCompatActivity {
 
   TextView userEmailTv;
-  TextView userPasswordTv;
+  TextView isCertEmailTv;
+
+  Button certBtn;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,28 @@ public class CertActivity extends AppCompatActivity {
     setContentView(R.layout.activity_cert);
 
     userEmailTv = findViewById(R.id.email_TV);
+    isCertEmailTv = findViewById(R.id.isCertTV);
+    certBtn = findViewById(R.id.certBtn);
 
     Intent intent = getIntent();
     String userID = intent.getStringExtra("userID");
+    userEmailTv.setText(userID);
 
-    userEmailTv.setText(userID.toString());
+    certBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        final String userEmail = userEmailTv.getText().toString();
+
+        //TODO: 이메일 인증부분
+        /* 이메일 발송 -> 랜덤한 코드 4자리 발송 */
+        /* 이메일 확인후 랜덤 코드 입력하면 인증 성공 */
+
+      }
+    });
+
+
+
+
 
   }
 }
